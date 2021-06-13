9a89eb92dcfc3e35db89119bd0f9b12d5a79afed
Ludovit Scholtz <ludko2@gmail.com>
2021-06-13 11:21:46 +0200

    update browser detect

--- src/misc/shimGetUserMedia.js
+++ src/misc/shimGetUserMedia.js
@@ -2,15 +2,14 @@ import { shimGetUserMedia as chromeShim } from "webrtc-adapter/src/js/chrome/get
 import { shimGetUserMedia as edgeShim } from "webrtc-adapter/src/js/edge/getusermedia";
 import { shimGetUserMedia as firefoxShim } from "webrtc-adapter/src/js/firefox/getusermedia";
 import { shimGetUserMedia as safariShim } from "webrtc-adapter/src/js/safari/safari_shim";
-import { detectBrowser } from "webrtc-adapter/src/js/utils";
+import adapter from "webrtc-adapter";
 
 import { StreamApiNotSupportedError } from "./errors";
 import { indempotent } from "./util";
 
 export default indempotent(() => {
-  const { browser } = detectBrowser(window);
-
-  switch (browser) {
+  console.log("adapter.browserDetails.browser", adapter.browserDetails.browser);
+  switch (adapter.browserDetails.browser) {
     case "chrome":
       chromeShim(window);
       break;
